a3014f10afce8dd6e71421a4341c1e4863909c06
Michael Nygard <mtnygard@cognitect.com>
2017-10-05 12:41:33 -0500

    Draft of Interceptor guide is complete

--- content/guides/what-is-an-interceptor/src/intc/service.clj
+++ content/guides/what-is-an-interceptor/src/intc/service.clj
@@ -1,18 +1,22 @@
+                                                                                     ;; tag::start[]
 (ns intc.service
   (:require [io.pedestal.http :as http]
             [io.pedestal.interceptor.chain :as chain]
             [io.pedestal.interceptor.error :as err]))
 
-                              ;; tag::hello[]
+(def routes #{})                                                                     ;; <1>
+
+                                                                                     ;; end::start[]
+                                                                                     ;; tag::hello[]
 (def say-hello
   {:name ::say-hello
    :enter (fn [context]
             (assoc context :response {:body "Hello, world!"
                                       :status 200}))})
-                              ;; end::hello[]
+                                                                                     ;; end::hello[]
 
 
-                              ;; tag::new_players[]
+                                                                                     ;; tag::new_players[]
 (def odds
   {:name ::odds
    :enter (fn [context]
@@ -24,28 +28,28 @@
    :enter (fn [context]
             (assoc context :response {:body "Even numbers are my bag\n"
                                       :status 200}))})
-                              ;; end::new_players[]
+                                                                                     ;; end::new_players[]
 
 
-                              ;; tag::chooser[]
+                                                                                     ;; tag::chooser[]
 (def chooser
   {:name  ::chooser
    :enter (fn [context]
             (try
-              (let [param (get-in context [:request :query-params :n]) ; <1>
-                    n     (Integer/parseInt param)               ;
-                    nxt   (if (even? n) evens odds)]             ;
-                (chain/enqueue context [nxt]))                   ; <2>
-              (catch NumberFormatException e                     ;
-                (assoc context :response {:body   "Not a number!\n" ; <3>
+              (let [param (get-in context [:request :query-params :n])               ;; <1>
+                    n     (Integer/parseInt param)
+                    nxt   (if (even? n) evens odds)]
+                (chain/enqueue context [nxt]))                                       ;; <2>
+              (catch NumberFormatException e
+                (assoc context :response {:body   "Not a number!\n"                  ;; <3>
                                           :status 400}))))})
 
 (def routes
   #{["/hello"        :get say-hello]
     ["/data-science" :get chooser]})
-                              ;; end::chooser[]
+                                                                                     ;; end::chooser[]
 
-                              ;; tag::chooser2[]
+                                                                                     ;; tag::chooser2[]
 (def chooser2
   {:name  ::chooser
    :enter (fn [context]
@@ -56,39 +60,41 @@
 (def routes
   #{["/hello"        :get say-hello]
     ["/data-science" :get chooser2]})
-                              ;; end::chooser2[]
+                                                                                     ;; end::chooser2[]
 
-                              ;; tag::error-i[]
+                                                                                     ;; tag::error-i[]
 (def number-format-handler
   {:name ::number-format-handler
    :error (fn [context exc]
-            (if (= :java.lang.NumberFormatException (:exception-type (ex-data exc)))    ; <1>
-                (assoc context :response {:body   "Not a number!\n" :status 400})   ; <2>
-                (assoc context :io.pedestal.interceptor.chain/error exc)))}) ; <3>
+            (if (= :java.lang.NumberFormatException (:exception-type (ex-data exc))) ;; <1>
+                (assoc context :response {:body   "Not a number!\n" :status 400})    ;; <2>
+                (assoc context :io.pedestal.interceptor.chain/error exc)))})         ;; <3>
 
 (def routes
   #{["/hello"        :get say-hello]
     ["/data-science" :get [number-format-handler chooser2]]})
 
-;; end::error-i[]
+                                                                                     ;; end::error-i[]
 
-;; tag::error-dispatch[]
+                                                                                     ;; tag::error-dispatch[]
 (def errors
   (err/error-dispatch [ctx ex]
 
-   [{:exception-type :java.lang.NumberFormatException}]                ;<1>
-   (assoc ctx :response {:status 400 :body "Not a number!\n"})))       ;<2>
+   [{:exception-type :java.lang.NumberFormatException}]                              ;; <1>
+   (assoc ctx :response {:status 400 :body "Not a number!\n"})))                     ;; <2>
 
 (def routes
   #{["/hello"        :get say-hello]
     ["/data-science" :get [errors chooser2]]})
-;; end::error-dispatch[]
+                                                                                     ;; end::error-dispatch[]
 
+                                                                                     ;; tag::run_service[]
 (defn start
   []
-  (-> {::http/port   8822     ;; <2>
-       ::http/join?  false    ;; <3>
-       ::http/type   :jetty   ;; <4>
-       ::http/routes routes}  ;; <5>
-      http/create-server      ;; <6>
-      http/start))            ;; <7>
+  (-> {::http/port   8822                                                            ;; <2>
+       ::http/join?  false                                                           ;; <3>
+       ::http/type   :jetty                                                          ;; <4>
+       ::http/routes routes}                                                         ;; <5>
+      http/create-server                                                             ;; <6>
+      http/start))                                                                   ;; <7>
+                                                                                     ;; end::run_service[]
