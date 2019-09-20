5b2e24d1fbb4d163991409e97067f143e3d048cf
Daniel De Aguiar <ddeaguiar@gmail.com>
2019-09-20 15:19:51 -0400

    Update what-is-an-interceptor project.

--- content/guides/what-is-an-interceptor-src/project.clj
+++ content/guides/what-is-an-interceptor-src/project.clj
@@ -1,8 +1,8 @@
-(defproject io.pedestal/what-is-an-interceptor "0.5.2"
+(defproject io.pedestal/what-is-an-interceptor "0.5.7"
   :description "Sample project for the What is an Interceptor guide"
   :url "https://github.com/pedestal/pedestal-docs"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.pedestal/pedestal.service "0.5.2"]
-                 [io.pedestal/pedestal.jetty "0.5.2"]
-                 [org.slf4j/slf4j-api "1.7.22"]
-                 [ch.qos.logback/logback-classic "1.1.8" :exclusions [[org.slf4j/slf4j-api]]]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.pedestal/pedestal.service "0.5.7"]
+                 [io.pedestal/pedestal.jetty "0.5.7"]
+                 [org.slf4j/slf4j-api "1.7.28"]
+                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [[org.slf4j/slf4j-api]]]])
