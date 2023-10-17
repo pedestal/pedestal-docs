515ed3c851bf0dafc1446ce42a0bab6f9a061eff
Howard M. Lewis Ship <hlship@gmail.com>
2023-10-17 15:41:06 -0700

    Move component example to pedestal repo

--- content/guides/what-is-an-interceptor-src/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject io.pedestal/what-is-an-interceptor "0.5.7"
-  :description "Sample project for the What is an Interceptor guide"
-  :url "https://github.com/pedestal/pedestal-docs"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.pedestal/pedestal.service "0.5.7"]
-                 [io.pedestal/pedestal.jetty "0.5.7"]
-                 [org.slf4j/slf4j-api "1.7.28"]
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [[org.slf4j/slf4j-api]]]])
