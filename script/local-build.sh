9aaa7b4aca8d17e48558bbcd4ad143e3530fe819
Howard M. Lewis Ship <hlship@gmail.com>
2023-10-18 16:06:51 -0700

    Fix a typo in the local-build.sh scipt

--- script/local-build.sh
+++ script/local-build.sh
@@ -2,8 +2,8 @@
 # Execute this from root directory.  You must have watchexec installed.
 set -exuo pipefail
 
-watchexec -c -\
-   -w ../pedestal/docs \
-   -w ui-overrides \
-   -w local-antora-playbook.yml \
+watchexec --clear --notify \
+   --watch ../pedestal/docs \
+   --watch ui-overrides \
+   --watch local-antora-playbook.yml \
    npx antora local-antora-playbook.yml
