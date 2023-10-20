7b1105533cac0daa8c53b532b857edd7febdb0d1
Howard M. Lewis Ship <hlship@gmail.com>
2023-10-20 10:41:33 -0700

    Add clj macro

--- script/local-build.sh
+++ script/local-build.sh
@@ -6,4 +6,6 @@ watchexec --clear --notify \
    --watch ../pedestal/docs \
    --watch ui-overrides \
    --watch local-antora-playbook.yml \
+   --watch lib \
+   --debounce=500ms \
    npx antora local-antora-playbook.yml
